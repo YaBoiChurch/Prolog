e33940e7005ed9d35d3534d93e52574d0b8d1cc8
YaBoiChurch <60941659+YaBoiChurch@users.noreply.github.com>
2020-05-02 20:26:53 -0500

    Add files via upload

---
+++ swish.pl
@@ -0,0 +1,9 @@
+state(arkansas). /* This is a simple fact that Arkansas is a state. Same with Florida and Texas*/
+state(florida).  /* The atoms in these facts are the state names*/
+state(texas).
+city(hotSprings, arkansas). /* This is a rule that states Hot Springs is a city in Arkansas*/
+city(littleRock, arkansas). /* This same rule applies to the cities in Texas and Florida*/
+city(dallas, texas).
+city(houston, texas).
+city(tampaBay, florida).
+city(jacksonville, florida).
